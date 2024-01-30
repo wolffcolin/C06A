4a34d41d62e8964553f82af50f6a2a4e81ca2d58
wolffcolin <cwcnsa@gmail.com>
2024-01-29 19:12:23 -0700

    Burner Class

--- src/Burner.java
+++ src/Burner.java
@@ -1,4 +1,28 @@
 
 public class Burner {
+	
+	public enum Tempurature {
+		BLAZING, HOT, WARM, COLD;
+	}
+	
+	private Tempurature myTempurature;
+	public Setting mySetting;
+	int timer;
+	public static final int TIME_DURATION = 2;
+	
+	public Burner() {
+		
+		this.myTempurature = Burner.Tempurature.COLD;
+		
+		this.mySetting = Setting.OFF;
 
+		this.timer = 0;
+		
+	}
+	
+	public void plusButton() {
+		
+	}
+	
+	
 }
