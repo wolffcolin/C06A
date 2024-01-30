19dd9da91b8ade5e8475ba5fc07c2c22d89092e7
wolffcolin <cwcnsa@gmail.com>
2024-01-29 20:36:36 -0700

    setting file final push

--- src/Setting.java
+++ src/Setting.java
@@ -1,3 +1,13 @@
+/*
+ 
+Class: Setting 
+Description: The Setting enum is just an enumerated type for the setting a stove is at. It has the options: OFF LOW
+MEDIUM HIGH 
+Authors: Colin Wolff and Matt McGrath
+Date: 1/29/2024
+Collaborators: N/A
+Sources: N/A
+*/
 
 public enum Setting {
 	OFF ("[---].....cooool"), LOW("[--+].....warm"), MEDIUM("[-++].....CAREFUL"), HIGH("[+++].....VERY HOT! DON'T TOUCH");
